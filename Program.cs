f9f394f1a9393e5e1c42fbab5c4e9ac904531d41
bstabs-alt <reubensweetmangough@gmail.com>
2022-10-17 12:04:23 +1300

    added print args

--- Program.cs
+++ Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 namespace NumGuess
 {
     internal class Program
     {
+        static int num = 0;
         static void Main(string[] args)
         {
+            if (args[1] != null) 
+            {
+                num = Int16.Parse(args[1]);
+            }
+            Console.WriteLine(num);
+            Environment.Exit(1);
         }
     }
 }
